59c318bf21974c6cf7d41876b6eefc7069a1e532
Landon Follows <lfollows@lcs.on.ca>
2019-10-21 15:25:15 -0400

    Created App, Having Technical Issues with Xcode app.

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,121 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     // MARK: Properties
     
-    // The Tip Amount
-    var tipPercent = Double(0.0)
+    @IBOutlet weak var AmountOfBill: UITextField!
     
-    // The Bill Amount
-    @IBOutlet weak var amountOfBill: UITextField!
+    @IBOutlet weak var SubmittedTipPercent: UITextField!
     
-    // The Amount of People
-    @IBOutlet weak var amountOfPeople: UITextField!
+    @IBOutlet weak var AmountOfPeople: UITextField!
     
-    // Total Tip Label
-    @IBOutlet weak var totalTipLabel: UILabel!
+    @IBOutlet weak var TotalTipLabel: UILabel!
     
-    // Tip Per Person Label
-    @IBOutlet weak var tipPerPersonLabel: UILabel!
+    @IBOutlet weak var TipPerPersonLabel: UILabel!
     
-    // Title Label
-    @IBOutlet weak var titleLabel: UILabel!
+    // Mark: Initializers
     
-    
-    //MARK: Methods
-    
+    // MARK: Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
-    }
     
-    func calculate() {
-        
-        // Take text field and turn them into strings
-        
-        // If Nothing In String, Tell Person to enter one
-        
-        guard let billAmountAsString = amountOfBill.text, amountOfBill.text != ""
-            
-        else {
-            titleLabel.text = " Please Enter All Values"
-            return
-        }
-        
-        // If Nothing In String, Tell Person to enter one
-        
-        guard let amountOfPeopleAsString = amountOfPeople.text, amountOfPeople.text != ""
-            
-        else{
-            titleLabel.text = "Please Enter All Values"
-            return
-        }
-        
-        // Take strings and turn them into doubles
-        
-        // If Unable to Convert to a Double, Show Message
-        
-        guard let billAmountAsDouble = Double(billAmountAsString)
-            
-        else{
-            titleLabel.text = "Enter Proper Values"
-            return
-        }
-        
-        // If Unable to Convert to a Double, Show Message
-        
-        guard let amountOfPeopleAsDouble = Double(amountOfPeopleAsString)
-        
-        else{
-            titleLabel.text = "Enter Usable Values"
-            return
-        }
-        
-        // Make Title Back
-        
-        titleLabel.text = "Tip Calculator"
-        
-        // Calculate tip amount
-        
-        let tipAmountInDollars = billAmountAsDouble * tipPercent
-        
-        // Round the value to 2 decimal places
-        
-        let roundedtipAmountInDollars = (round(100*tipAmountInDollars)/100)
-        
-        // Change tip amount into a string
-        
-        let tipAmountInDollarsasString = String(roundedtipAmountInDollars)
-        
-        // Share information to the user Main
-        
-        totalTipLabel.text = "Total Tip: \(tipAmountInDollarsasString)$"
-        
-        // Calculate tip per person
-        
-        let tipAmountPerPerson = tipAmountInDollars/amountOfPeopleAsDouble
-        
-        // Round the value to 2 decimal places
-        
-        let roundedTipPerPerson = (round(100*tipAmountPerPerson)/100)
-        
-        // Change tip per person into a string
-        
-        let tipPerPersonasString = String(roundedTipPerPerson)
-        
-        // Share information to the user interface
-        
-        tipPerPersonLabel.text = "Tip Per Person: \(tipPerPersonasString)$"
-        
-        
-    }
-    
-    @IBAction func tipPercent(_ sender: Any) {
-        tipPercent = "Double"
-        calculate()
-      }
-      
-}
 
+    }
+    
+    
+    @IBAction func CalculateTheTip(_ sender: Any) {
+        
+        // Get the user input
+        
+        let BillText = AmountOfBill.text!
+        
+        let TipPercentText = SubmittedTipPercent.text!
+        
+        let AmountOfPeopleText = AmountOfPeople.text!
+        
+        // Convert all values into Double
+        
+        let TipInPercent = Double(TipPercentText)!
+        let Bill = Double(BillText)!
+        let AmountOfPeople = Double(AmountOfPeopleText)!
+        
+        // Calculate the Tip and the tip per person
+        
+        let TotalTip = Bill*(TipInPercent/100)
+        
+        let TipPerPerson = TotalTip / AmountOfPeople
+        
+        // Send the results to the view
+        
+        // TotalTipLabel.text = String(TotalTip)
+        
+        // TotalTipLabel.text = "$\(TotalTip)"
+        
+        TotalTipLabel.text = String(format:   "$%.2f ", TotalTip)
+        
+        // TipPerPersonLabel.text = String(TipPerPerson)
+        
+        // TipPerPersonLabel.text = "$\(TipPerPerson)"
+        
+        TipPerPersonLabel.text = String(format: "$%.2f", TipPerPerson)
+        
+    }
+    
+    
+    
+   
+}
